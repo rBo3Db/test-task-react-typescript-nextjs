0bb1a0ee0357b18c24692fec2d486d175277c572
rBo3Db <rBo3Db@mail.ru>
2020-01-27 19:14:39 +0400

    sorry i am stuped

---
+++ _next/static/PhoxVLdXPpsK1iSAFuNz0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/payPage":[a]}}("static\u002Fchunks\u002F8df1c799a7456e405443c4dc59786be647cf1a2d.dfb680ef2a0fba609a83.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
