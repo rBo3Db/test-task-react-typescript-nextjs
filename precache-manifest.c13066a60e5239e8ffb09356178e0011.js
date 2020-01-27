50692019c7366d123645b32761688b3d6210343c
rBo3Db <rBo3Db@mail.ru>
2020-01-27 19:08:43 +0400

    ups

---
+++ precache-manifest.c13066a60e5239e8ffb09356178e0011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd5243396b74bab8013b9fbeec670aa",
+    "url": "/brave-test-on-react-dev/index.html"
+  },
+  {
+    "revision": "1afe4f8bb0fda0f7d496",
+    "url": "/brave-test-on-react-dev/static/js/2.bd9964f4.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/brave-test-on-react-dev/static/js/2.bd9964f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0365af651f8cc185006d",
+    "url": "/brave-test-on-react-dev/static/js/main.3147becd.chunk.js"
+  },
+  {
+    "revision": "b92cded757897a30a66b",
+    "url": "/brave-test-on-react-dev/static/js/runtime-main.489aa729.js"
+  }
+]);
